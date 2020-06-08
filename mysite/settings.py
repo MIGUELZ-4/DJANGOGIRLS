fc366dc7cc64dfa494289298e4ad360a475e3c1b
MIGUELZ-4 <arcangel5901@hotmail.com>
2020-06-08 17:25:56 -0500

    mi primer commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fe_$1+%dsclt_^jp^q4r^qu-xpwom#z8m+m+i$&71ii=t8p85^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost' ,'127.0.0.1']
+ALLOWED_HOSTS = ['localhost' ,'pythonanywhere.com']
 
 
 # Application definition
